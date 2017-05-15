dc17a190f52b52894fdc6c6c331cbd1f5c66d16c
github3823 <gg47549390@gmail.com>
2017-05-16 00:02:55 +0800

    Update 0011.py

--- 0011.py
+++ 0011.py
@@ -1,17 +1,5 @@
 #https://github.com/Yixiaohan/show-me-the-code
-'''第 0011 题： 敏感词文本文件 filtered_words.txt，里面的内容为以下内容，当用户输入敏感词语时，则打印出 Freedom，否则打印出 Human Rights。
-
-北京
-程序员
-公务员
-领导
-牛比
-牛逼
-你娘
-你妈
-love
-sex
-jiangge'''
+'''第 0012 题： 敏感词文本文件 filtered_words.txt，里面的内容 和 0011题一样，当用户输入敏感词语，则用 星号 * 替换，例如当用户输入「北京是个好城市」，则变成「**是个好城市」。'''
 # coding:utf-8
 i=['北京','程序员','公务员','领导','牛比','牛逼','你娘','你妈','love','sex','jiangge']#
 b='北京程序员人公务员不是人呀牛，了比，牛逼，你妈，你大娘，lovesexjiangge'
